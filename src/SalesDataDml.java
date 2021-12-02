25366cc775e39aef5b4296c0bc33fc2503a44de8
DELL <twinkledahale98@gmail.com>
2021-12-02 12:30:14 +0530

    Task Continue

--- src/SalesDataDml.java
+++ src/SalesDataDml.java
@@ -1,18 +1,70 @@
+import java.sql.*;
 import java.util.concurrent.Callable;
 
-public class SalesDataDml
-{
-    public static void main(String args[])
+public class SalesDataDml {
+    public static void main(String args[]) {
+        SalesDataDml sd = new SalesDataDml();
+        sd.insertSales();
+        sd.deleteSales();
+    }
+    public void insertSales ()
+    {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Registered ..");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sales_data", "root", "root");
+            System.out.println("Connection Established..");
+
+            String query = "insert into sales_person values(?,?,?,?)";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setInt(1, 1010);
+            pst.setString(2, "john_kenov");
+            pst.setString(3, "Nordic");
+            pst.setInt(4, 200);
+
+            int res = pst.executeUpdate();
+            System.out.println("Result" + res);
+            if (res == 0)
+                System.out.println("No Record Inserted");
+            else
+                System.out.println("Records Inserted");
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public void deleteSales()
     {
         try
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver Registered ..");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sales_data", "root", "root");
+            System.out.println("Connection Established..");
 
-
+            String first_name="axel_rod";
+            String query = "delete from sales_person where first_name=?";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setString(1,first_name);
+            int rs = pst.executeUpdate();
+            if(rs==0)
+            {
+                System.out.println("Record Not Deleted");
             }
-        catch (Exception e){
+            else
+            {
+                System.out.println("Record Deleted");
+            }
+
+        }
+        catch(Exception e)
+        {
             System.out.println(e);
-        };
+        }
     }
+
+    public
 }
