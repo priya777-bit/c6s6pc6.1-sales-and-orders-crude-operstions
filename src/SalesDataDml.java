ad97c4858c82a3f4ac6f536cfa3d01455db4e809
DELL <twinkledahale98@gmail.com>
2021-12-02 17:33:31 +0530

    Task Completed

--- src/SalesDataDml.java
+++ src/SalesDataDml.java
@@ -13,17 +13,17 @@ public class SalesDataDml {
         } catch (Exception e) {
             System.out.println(e);
         }
-        OrderData od = new OrderData(3015,3000,dt,2010);
+        //OrderData od = new OrderData(3015,3000,dt,2010);
         //SalesData sd = new SalesData(1012,"john_kenov","nordic",200);
         //CustomerData cd = new CustomerData(2010,"samuel","thomas","stockholm",150,1011);
         //boolean result = dbms.insertSales(sd);
-        //boolean result1 = dbms.deleteSales(1009);
+        boolean result1 = dbms.deleteSales(1001);
         //boolean result2 = dbms.insertCustomerOrder(cd);
-        boolean result3 = dbms.insertCustomerOrder2(od);
+        //boolean result3 = dbms.insertCustomerOrder2(od);
 
-        OrderData od1 = dbms.getCustomerId(2010);
-        od1.setAmount(3500);
-        od1.setCustomer_id(2010);
-        boolean result4 = dbms.updateOrderAmount(od1);
+        //OrderData od1 = dbms.getCustomerId(2010);
+        //od1.setAmount(3500);
+        //od1.setCustomer_id(2010);
+        //boolean result4 = dbms.updateOrderAmount(od1);
     }
 }
