d026e287e0a9af8417aabd4a8154bbd1e92a2dae
DELL <twinkledahale98@gmail.com>
2021-12-02 17:17:01 +0530

    Task Completed

--- src/SalesDataDml.java
+++ src/SalesDataDml.java
@@ -6,20 +6,20 @@ public class SalesDataDml {
     public static void main(String args[]) {
         DbOperations dbms = new DbOperations();
         String date ="2021-05-15";
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         java.util.Date dt = null;
         try {
             dt = sdf.parse(date);
-        } catch (ParseException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        //OrderData od = new OrderData(3014,3000,dt,2010);
+        OrderData od = new OrderData(3015,3000,dt,2010);
         //SalesData sd = new SalesData(1012,"john_kenov","nordic",200);
         //CustomerData cd = new CustomerData(2010,"samuel","thomas","stockholm",150,1011);
         //boolean result = dbms.insertSales(sd);
         //boolean result1 = dbms.deleteSales(1009);
         //boolean result2 = dbms.insertCustomerOrder(cd);
-        //boolean result3 = dbms.insertCustomerOrder2(od);
+        boolean result3 = dbms.insertCustomerOrder2(od);
 
         OrderData od1 = dbms.getCustomerId(2010);
         od1.setAmount(3500);
