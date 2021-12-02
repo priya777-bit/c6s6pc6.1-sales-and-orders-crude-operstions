b402d075b8ad41e00631aeef87e803f12f289ac1
DELL <twinkledahale98@gmail.com>
2021-12-02 13:08:45 +0530

    Task Continue

--- src/SalesDataDml.java
+++ src/SalesDataDml.java
@@ -1,11 +1,12 @@
 import java.sql.*;
-import java.util.concurrent.Callable;
+import java.text.SimpleDateFormat;
 
 public class SalesDataDml {
     public static void main(String args[]) {
         SalesDataDml sd = new SalesDataDml();
         sd.insertSales();
         sd.deleteSales();
+        sd.insertCustomerOrder();
     }
     public void insertSales ()
     {
@@ -66,5 +67,49 @@ public class SalesDataDml {
         }
     }
 
-    public
+    public void insertCustomerOrder()
+    {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Registered ..");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sales_data", "root", "root");
+            System.out.println("Connection Established..");
+
+            String query = "insert into customer values(?,?,?,?,?,?)";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setInt(1, 2008);
+            pst.setString(2, "samuel");
+            pst.setString(3, "thomas");
+            pst.setString(4, "stockholm");
+            pst.setInt(5, 150);
+            pst.setInt(6, 1010);
+
+            int res = pst.executeUpdate();
+            if (res == 0) {
+                System.out.println("Customer Record Not Inserted..");
+            } else {
+                System.out.println("Customer Record Inserted");
+            }
+
+            String date1 = "15/05/2021";
+            //java.util.Date date = new SimpleDateFormat("dd/MM/yyyy").parse(date1);
+            String query2 = "insert into order_details values(?,?,?,?)";
+            PreparedStatement pst2 = con.prepareStatement(query2);
+            pst2.setInt(1, 3012);
+            pst2.setInt(2, 3000);
+            pst2.setDate(3, new java.sql.Date(new SimpleDateFormat("dd/MM/yyyy").parse(date1).getTime()));
+            pst2.setInt(4, 2008);
+
+            int res2 = pst2.executeUpdate();
+            if (res2 == 0) {
+                System.out.println("No Record Inserted In Order Details..");
+            } else {
+                System.out.println("Record Inserted In Order Details..");
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
 }
